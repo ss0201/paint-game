f3452e0a84bc4665bb9dcd8ccd68faddd50a8cb5
ss0201 <ss0201.777@gmail.com>
2013-11-09 07:09:01 +0900

    Change the duration of answer phase

--- models.js
+++ models.js
@@ -3,7 +3,7 @@ function Game (name, problemSetId, drawingPhaseDuration, guessingPhaseDuration) 
   self.name = name;
   self.drawingPhase = new Phase("Drawing", drawingPhaseDuration);
   self.guessingPhase = new Phase("Guessing", guessingPhaseDuration);
-  self.answerPhase = new Phase("Answer", 5);
+  self.answerPhase = new Phase("Answer", 10);
   self.phase = self.drawingPhase;
   self.clock = self.phase.duration;
   self.problemSetId = problemSetId;
